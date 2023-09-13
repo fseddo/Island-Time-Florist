3fe0c32a3175456b96c2601db00677b113f47ad6
Francesco <francesco.seddo@gmail.com>
2023-09-12 21:55:08 -0400

    updated axios

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,32 +1,31 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const axios = require('axios');
-const Order = require('./Order')
+const Sequelize = require("sequelize");
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Order = require("./Order");
 
 const SALT_ROUNDS = 5;
 
-const User = db.define('user', {
+const User = db.define("user", {
   username: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   password: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
       notEmpty: true,
-      len: [6,128]
-    }
+      len: [6, 128],
+    },
   },
   isAdmin: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false
+    defaultValue: false,
   },
   email: {
     type: Sequelize.STRING,
@@ -34,70 +33,69 @@ const User = db.define('user', {
     allowNull: false,
     validate: {
       notEmpty: true,
-      isEmail: true
-    }
-  }
+      isEmail: true,
+    },
+  },
+});
 
-})
-
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
+User.prototype.correctPassword = function (candidatePwd) {
   //we need to compare the plain version to an encrypted version of the password
   return bcrypt.compare(candidatePwd, this.password);
-}
+};
 
-User.prototype.generateToken = function() {
-  return jwt.sign({id: this.id}, process.env.JWT)
-}
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
 
 /**
  * classMethods
  */
-User.authenticate = async function({ username, password }){
-    const user = await this.findOne({where: { username }})
-    if (!user || !(await user.correctPassword(password))) {
-      const error = Error('Incorrect username/password');
-      error.status = 401;
-      throw error;
-    }
-    return user.generateToken();
+User.authenticate = async function ({ username, password }) {
+  const user = await this.findOne({ where: { username } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error("Incorrect username/password");
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
 };
 
-User.findByToken = async function(token) {
+User.findByToken = async function (token) {
   try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = User.findByPk(id)
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
     if (!user) {
-      throw 'bad credentials'
+      throw "bad credentials";
     }
-    return user
+    return user;
   } catch (ex) {
-    const error = Error('bad credentials')
-    error.status = 401
-    throw error
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
   }
-}
+};
 
 /**
  * hooks
  */
-const hashPassword = async(user) => {
+const hashPassword = async (user) => {
   //in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
+  if (user.changed("password")) {
     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
   }
-}
+};
 
-const setOrder = async(user) => {
-  const initOrder = await Order.create()
-  initOrder.setUser(user.id)
-}
+const setOrder = async (user) => {
+  const initOrder = await Order.create();
+  initOrder.setUser(user.id);
+};
 
-User.beforeCreate(hashPassword)
-User.beforeUpdate(hashPassword)
-User.afterCreate(setOrder)
-User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+User.beforeCreate(hashPassword);
+User.beforeUpdate(hashPassword);
+User.afterCreate(setOrder);
+User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
